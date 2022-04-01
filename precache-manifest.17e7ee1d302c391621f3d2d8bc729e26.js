53c1e5832239de9a3348a33de5e817becc8ec0c4
LiubovSchmit <liubou.schmit@gmail.com>
2022-04-01 16:47:27 +0200

    Updates

---
+++ precache-manifest.17e7ee1d302c391621f3d2d8bc729e26.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f07ccde5d7cfa8182f8735e1699dc72",
+    "url": "/todolist_app/index.html"
+  },
+  {
+    "revision": "c8bcf12b25ca1f076eb5",
+    "url": "/todolist_app/static/css/main.9d943d0d.chunk.css"
+  },
+  {
+    "revision": "6c83a146cb9ed4c00b3f",
+    "url": "/todolist_app/static/js/2.8c814fb5.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/todolist_app/static/js/2.8c814fb5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8bcf12b25ca1f076eb5",
+    "url": "/todolist_app/static/js/main.a4914160.chunk.js"
+  },
+  {
+    "revision": "efea4bf8a3efccd1ac52",
+    "url": "/todolist_app/static/js/runtime-main.dbee106a.js"
+  }
+]);
